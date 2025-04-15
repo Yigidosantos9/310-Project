a0324542518603f236e0df3ef95af05225e19345
Nikomik48 <keremxk@gmail.com>
2025-04-15 17:36:37 +0300

    Constants arranged and custom widgets have been added.

---
+++ spacy_notes/lib/CustomWidgets/customText.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:spacy_notes/core/constants/color_constants.dart';
+
+class CustomText extends StatelessWidget {
+  final String text;
+  final Color color;
+  final double fontSize;
+  final FontWeight fontWeight;
+  final String fontFamily;
+  final TextAlign textAlign;
+  final int? maxLines;
+  final TextOverflow overflow;
+
+  const CustomText({
+    Key? key,
+    required this.text,
+    this.maxLines,
+    this.fontFamily = 'Jersey',
+    this.color = AppColors.darkSubTextColor,
+    this.fontSize = 23.0,
+    this.fontWeight = FontWeight.normal,
+    this.textAlign = TextAlign.start,
+    this.overflow = TextOverflow.ellipsis,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: TextStyle(
+        color: color,
+        fontSize: fontSize,
+        fontWeight: fontWeight,
+      ),
+      textAlign: textAlign,
+      maxLines: maxLines,
+      overflow: overflow,
+    );
+  }
+}
--- spacy_notes/lib/core/constants/color_constants.dart
+++ spacy_notes/lib/core/constants/color_constants.dart
@@ -10,31 +10,39 @@ class AppColors {
   // so this changes affect the entire application
 
   // primary Color of the app 
-  static const Color primary = Color(0xFF0D0D0D);  
+  static const Color primary = Color(0xFF0C0C0C);  
   // Color that on the primary color (Texts, icons ...) 
-  static const Color onPrimary = Colors.white; 
+  static const Color onPrimary = Color(0xFFD9D9D9); 
 
   // Secondary color of the app
-  static const Color secondary = Color(0xFF1E88E5);   
+  static const Color secondary = Color(0xFFF7931B);   
   // Color that on the secondary color (Texts, icons ...) 
   static const Color onSecondary = Colors.white;
 
   // General background color
-  static const Color background = Color(0xFF121212);   
+  static const Color background = Color(0xFF0C0C0C);   
 
   // General error colors  
   static const Color error = Colors.redAccent;  
   static const Color onError = Colors.white;   
 
   // General surface theme  
-  static const Color surface = Color(0xFF1C1C1C);      
+  static const Color surface = Color(0xFF0C0C0C);      
   static const Color onSurface = Colors.white70; 
 
   // General app theme (dark or bright now)     
   static const Brightness brightness = Brightness.dark; 
 
-  static const Color buttonColor=Color(0xFF8C12DC);
+  // General colors for the app
+  static const Color mainButtonColor=Color(0xFF8C12DC);
+  static const Color secondaryButtonColor=Color(0xFFC4C4C4);
+  static const Color iconColor=Color(0xFFf19ddc);
+  static const Color selectedTaskColor=Color(0xFFD9D9D9);
+  static const Color unselectedTaskColor=Color(0xFF8C898E);
 
+  static const Color mainTextColor=Color(0xFFFFFFFF);
+  static const Color darkSubTextColor=Color(0xFF5A5A5A);
+  static const Color lightSubTextColor=Color(0xFFC4C4C4);
 
   // With this you can use this class without create an instance
   AppColors._();
