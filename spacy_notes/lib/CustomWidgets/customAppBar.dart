9b8e94e80d167d02190c9ba789ab760d57eb69ad
Nikomik48 <keremxk@gmail.com>
2025-04-18 17:00:48 +0300

    New appbar style have been created

--- spacy_notes/lib/CustomWidgets/customAppBar.dart
+++ spacy_notes/lib/CustomWidgets/customAppBar.dart
@@ -4,25 +4,27 @@ import 'package:spacy_notes/core/constants/color_constants.dart';
 
 class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   final String title;
+  final String? subTitle;
   final VoidCallback? onPressed;
   final Widget? rhs;
 
   const CustomAppBar({
     super.key,
     this.rhs, // This is an optional widget that can be placed on the right side of the AppBar.
+    this.subTitle, // This is an optional subtitle.
     required this.title, // This is the required title parameter.
     this.onPressed, //This is an optional callback for the back button.
   });
 
   @override
-  Size get preferredSize => const Size.fromHeight(100);
+  Size get preferredSize => const Size.fromHeight(60);
 
   @override
 Widget build(BuildContext context) {
-  return PreferredSize(
+  return /*PreferredSize(
     preferredSize: preferredSize,
     child: Container(
-      color: AppColors.primary,
+      color: AppColors.appBarColor,
       padding: const EdgeInsets.only( left: 16, right: 16),
       child: Stack(
         alignment: Alignment.center,
@@ -62,5 +64,65 @@ Widget build(BuildContext context) {
       ),
     ),
   );
-}
-}
+}*/AppBar(
+        backgroundColor: const Color.fromARGB(255, 17, 1, 37),
+        leading: const CloseButton(),
+        title: Center(
+          child: RichText(
+            text: TextSpan(
+              children: [
+                // 'Spacy ' kısmı gradient ile
+                WidgetSpan(
+                  child: ShaderMask(
+                    shaderCallback:
+                        (bounds) => const LinearGradient(
+                          colors: [Color(0xFF5B2C6F), Color(0xFF9B59B6)],
+                        ).createShader(bounds),
+                    child: Text(
+                      title,
+                      style: TextStyle(
+                        fontFamily: 'Orbitron',
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+                // 'Store' kısmı da gradient veya beyaz olarak
+                WidgetSpan(
+                  child: ShaderMask(
+                    shaderCallback:
+                        (bounds) => const LinearGradient(
+                          colors: [
+                            Color.fromARGB(255, 232, 229, 233),
+                            Color.fromARGB(255, 255, 255, 255),
+                          ],
+                        ).createShader(bounds),
+                    child: Text(
+                      subTitle!=null ? " $subTitle":'',
+                      style: TextStyle(
+                        fontFamily: 'Orbitron',
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 12),
+            child: Row(
+              children: [
+                rhs?? SizedBox(),
+              ],
+            ),
+          ),
+        ],
+      );
+}}
